978fbe14c8a93a47b273484cbd65aa20abb3e88d
VladGribinchuk <vladgribinchook@gmail.com>
2015-03-26 15:11:06 +0200

    Lexical analyzer (uses Flex)
    designed by flex to GNU/Linux

---
+++ LexAnalyzer/scan_header.h
@@ -0,0 +1,34 @@
+#define _CONST_STAT	1
+#define _VAR_STAT 	2
+#define _PROC_STAT	3
+#define _CALL_STAT 	4
+#define _BEG_STAT 	5
+#define _END_STAT 	6
+#define _DO_STAT 	7
+#define _IF_STAT 	8
+#define _THEN_STAT 	9
+#define _WHILE_STAT 	10
+
+#define _SEMICOLON 	11
+#define _COLON 		12
+#define _COMMA 		13
+#define _DOT 		14
+
+#define _PLUS 		15
+#define _MINUS 		16
+#define _DIRECT_SLASH 	17
+#define _TIMES 		18
+#define _LPAREN 	19
+#define _RPAREN 	20
+
+#define _ASSIGN 	21
+#define _EQ 		22
+#define _NEQ 		23
+#define _GT 		24
+#define _LT 		25
+#define _GE 		26
+#define _LE 		27
+
+#define _ID 		100
+#define _NUM 		101
+#define _UNKNOWN_SYMBOL	200
